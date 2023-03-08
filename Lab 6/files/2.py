80923bc6c2c8a47df6c2f9ef1a31821251147c9e
DosbolBolat <122983530+DosbolBolat@users.noreply.github.com>
2023-03-08 23:37:57 +0600

    Delete Lab 6/files directory

--- Lab 6/files/2.py
+++
@@ -1,9 +0,0 @@
-import os
-name_file = input()
-
-p = os.path.abspath (name_file)
-print(p)
-print(os.access(p, os.F_OK))
-print(os.access(p, os.R_OK))
-print(os.access(p, os.W_OK))
-print(os.access(p, os.X_OK))
--- Lab 6/files/6.py
+++
@@ -1,6 +0,0 @@
-def file_generator():
-    for i in range(26):
-        filename = chr(65 + i)
-        open(f"{filename}.txt", "x")
-        
-file_generator()
