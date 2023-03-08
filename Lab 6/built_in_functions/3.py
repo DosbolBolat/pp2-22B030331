c52a743108d04ceddea0618f832d7ca0bb702f39
DosbolBolat <122983530+DosbolBolat@users.noreply.github.com>
2023-03-08 23:37:47 +0600

    Delete Lab 6/built_in_functions directory

--- Lab 6/built_in_functions/3.py
+++
@@ -1,8 +0,0 @@
-#Write a Python program with builtin function that checks whether a passed string is palindrome or not.
-
-def isItPolindrom(s):
-    return s == "".join(reversed(s))
-
-s = input()
-
-print(isItPolindrom(s))
