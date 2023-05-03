f22811aec7307ac548fc34400b920a5218af70d6
DosbolBolat <122983530+DosbolBolat@users.noreply.github.com>
2023-05-03 14:47:35 +0600

    Add files via upload

---
+++ lab 11/query_pagination.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION query_pagination(query_value VARCHAR(20), lim INT, offst INT)
+    RETURNS TABLE(id INT, name VARCHAR(20), number VARCHAR(20))
+	AS $$ BEGIN
+		RETURN QUERY
+			SELECT * FROM PhoneBook WHERE
+			PhoneBook.name ILIKE '%' || query_value || '%' OR PhoneBook.number ILIKE '%' || query_value || '%'
+			ORDER BY PhoneBook.id LIMIT lim OFFSET offst;
+			
+    END; $$
+	LANGUAGE plpgsql
